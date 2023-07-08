3a850c856d997fe1c71b6497fbcd3159156eea27
Rishi838 <rishidhingra04@gmail.com>
2023-07-08 23:48:01 +0530

    Laying down the structure

--- server.js
+++ server.js
@@ -1,5 +1,28 @@
 const express = require('express')
-const app=express()
-app.listen(3000,(req,res)=>{
-    console.log("Listening at port 3000");
+const path = require('path')
+const authRoutes = require('./routes/authroutes')
+
+
+const app = express()
+// Using this middleware to parse req.body in json format
+app.use(express.json())
+
+// setting views and view engine
+
+// Setting view engine tp ejs files
+app.set("view engine", "ejs")
+// setting path for ejs files
+app.set("views", path.join("./views"))
+// Setting path to render css
+app.use(express.static(__dirname));
+
+const port = process.env.PORT || 8080
+
+
+// Using auth routes in our server files which take access from routes folder
+app.use(authRoutes)
+
+// Listening on port defined in .env files else on port 8080
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
 })
