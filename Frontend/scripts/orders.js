34897dd80285415c7540a69fc347c4d9c7f0390b
Rishi838 <rishidhingra04@gmail.com>
2023-07-28 16:28:05 +0530

    Frontend Done

--- Frontend/scripts/orders.js
+++ Frontend/scripts/orders.js
@@ -1,5 +1,33 @@
 import { postData } from "../frontend_utils/fetch_api.js";
 
+const navToggler = document.querySelector('.nav-toggler');
+const navMenu = document.querySelector('.site-navbar ul');
+const navLinks = document.querySelectorAll('.site-navbar a');
+
+// load all event listners
+allEventListners();
+
+// functions of all event listners
+function allEventListners() {
+  // toggler icon click event
+  navToggler.addEventListener('click', togglerClick);
+  // nav links click event
+  navLinks.forEach( elem => elem.addEventListener('click', navLinkClick));
+}
+
+// togglerClick function
+function togglerClick() {
+  navToggler.classList.toggle('toggler-open');
+  navMenu.classList.toggle('open');
+}
+
+// navLinkClick function
+function navLinkClick() {
+  if(navMenu.classList.contains('open')) {
+    navToggler.click();
+  }
+}
+
 async function validate_user() {
   const result = await postData("/validate", {});
   if (result.validate == 1) {
